2a483f6a4fb7a3e5c9318c2f74fc682713d0b2b4
Fabiano Nalin <fnalin@msn.com>
2017-05-15 16:05:02 -0300

    Add FluentValidator

---
+++ backend/FN.CadRestaurante/FN.CadRestaurante.Domain/Entities/_Entity.cs
@@ -0,0 +1,17 @@
+﻿using FN.CadRestaurante.Domain.FluentValidator;
+using System;
+
+namespace FN.CadRestaurante.Domain.Entities
+{
+    public abstract class Entity : Notifiable
+    {
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+            DataCadastro = DateTime.Now;
+        }
+        public Guid Id { get; private set; }
+        public DateTime DataCadastro { get; private set; }
+
+    }
+}
---
+++ backend/FN.CadRestaurante/FN.CadRestaurante.Domain/FluentValidator/Notification.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FN.CadRestaurante.Domain.FluentValidator
+{
+    public class Notification
+    {
+        public Notification(string property, string message)
+        {
+            Id = Guid.NewGuid();
+            Property = property;
+            Message = message;
+        }
+        public Guid Id { get; private set; }
+        public string Property { get; private set; }
+        public string Message { get; private set; }
+    }
+}
