fcf5bc136631411f7d73475a5065c76926bdb258
Fabiano Nalin <fnalin@msn.com>
2017-05-15 16:19:17 -0300

    Add entities

---
+++ backend/FN.CadRestaurante/FN.CadRestaurante.Domain/Entities/Prato.cs
@@ -0,0 +1,28 @@
+﻿using FN.CadRestaurante.Domain.FluentValidator;
+
+namespace FN.CadRestaurante.Domain.Entities
+{
+    public class Prato : Entity
+    {
+        protected Prato() { }
+
+        public Prato(string nome, decimal preco)
+        {
+            Nome = nome;
+            Preco = preco;
+            validar();
+        }
+
+        public string Nome { get; private set; }
+        public decimal Preco { get; private set; }
+
+        private void validar()
+        {
+            new ValidationContract<Prato>(this)
+                .IsRequired(p => p.Nome, "O nome do prato é obrigatório")
+                .HasMaxLenght(p => p.Nome, 100, "O nome do prato deve ter até 100 caracteres")
+                .HasMinLenght(p => p.Nome, 3, "O nome do prato deve ter ao menos 3 caracteres")
+                .IsGreaterThan(p => p.Preco, 0M, "O preço deve ser acima de 0");
+        }
+    }
+}
---
+++ backend/FN.CadRestaurante/FN.CadRestaurante.Domain/Entities/Restaurante.cs
@@ -0,0 +1,26 @@
+﻿using FN.CadRestaurante.Domain.FluentValidator;
+
+namespace FN.CadRestaurante.Domain.Entities
+{
+    public class Restaurante : Entity
+    {
+        protected Restaurante()
+        {}
+
+        public Restaurante(string nome)
+        {
+            Nome = nome;
+            validar();
+        }
+
+        public string Nome { get; private set; }
+
+        private void validar()
+        {
+            new ValidationContract<Restaurante>(this)
+                .IsRequired(p => p.Nome, "O nome do reaturante é obrigatório")
+                .HasMaxLenght(p => p.Nome, 100, "O nome do restaurante deve ter até 100 caracteres")
+                .HasMinLenght(p => p.Nome, 3, "O nome do restaurante deve ter ao menos 3 caracteres");
+        }
+    }
+}
