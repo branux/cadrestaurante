1c8374056615690fa23f3b01d20489c2a79ac03f
Fabiano Nalin <fnalin@msn.com>
2017-05-16 11:10:28 -0300

    Add connString e config. IoC na api

--- backend/FN.CadRestaurante/FN.CadRestaurante.Api/Startup.cs
+++ backend/FN.CadRestaurante/FN.CadRestaurante.Api/Startup.cs
@@ -1,21 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using FN.CadRestaurante.CrossCutting.IoC;
 
 namespace FN.CadRestaurante.Api
 {
     public class Startup
     {
+        private IConfigurationRoot Configuration { get; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                //.AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
             services.AddCors();
+            services.AddSingleton(Configuration);
+            IoCConfiguration.Configure(services);
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
